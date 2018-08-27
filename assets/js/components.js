33c3e7b2229f3c119be79e2c071990dfc4093ea3
Gitlab CI <GitLab@turn.lv>
2018-08-27 09:00:54 +0000

    Changes from GitLab

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,69 +1,26 @@
-var ScrollIndicator = (function () {
-  var progressBar = $('.scroll-indicator__bar') 
-  var debug = false
-  var value = 0
-  var max
-  var width        
-
-  var getMax = function(){
-    return $('.fp-section').length - 1
-  }
-
-  var getWidth = function(){
-    // Calculate width in percentage
-    width = (value/max) * 100;
-    width = width + '%';
-    return width;
-  }
-    
-  var setWidth = function(){
-    progressBar.css({ width: getWidth() });
-  }
-
-  return {
-    init: function () {
-      if (!window.jQuery) {
-        console.warn('Error: jQuery is not loaded')
-        return
-      }
-      if (debug) console.info('ScrollIndicator initialized')
-      max = getMax()
-    },
-    update: function (section) {
-      value = section
-      setWidth() 
-    }
-  }
-})()
-
-$(document).ready( function() {
-  ScrollIndicator.init()
-})
-
-
 var Slidenav = (function () {
-    var debug = false
-    var prev = $('.slidenav__prev')
-    var next = $('.slidenav__next')
+    var debug = false;
+    var prev = $('.slidenav__prev');
+    var next = $('.slidenav__next');
 
     var checkDependencies = function () {
         if (!window.jQuery) {
-            console.warn('Error: jQuery is not loaded')
+            console.warn('Error: jQuery is not loaded');
             return
         }
-    }
+    };
 
     var enable = function (elem) {
         if (elem.hasClass('u-disabled')) {
             elem.removeClass('u-disabled')
         }
-    }
+    };
 
     var disable = function (elem) {
         if (!elem.hasClass('u-disabled')) {
             elem.addClass('u-disabled')
         }
-    }
+    };
 
     return {
         enablePrev: function (boolean) {
@@ -86,4 +43,4 @@ var Slidenav = (function () {
             }
         }
     }
-})()
+})();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 (function ($) {
     $( document ).ready(function() {
-        var $htmlBody = $('html, body'),
-            $body = $('body'),
-            $burger = $('.js-burger'),
+        ScrollIndicator.init();
+
+        var $burger = $('.js-burger'),
             $menu = $('.js-header-menu'),
             $tweetBtn = $('.js-tweet'),
             $fbBtn = $('.js-fb'),
@@ -23,7 +23,6 @@
         $fbBtn.on('click', function(e){
             e.preventDefault();
 
-            console.log(themeUrl + '/assets/img/landing.jpg');
             FB.ui({
                 method: 'share_open_graph',
                 action_type: 'og.shares',
@@ -40,3 +39,89 @@
         });
     });
 })(jQuery);
+
+
+var ScrollIndicator = (function () {
+    var progressBar = $('.scroll-indicator__bar');
+    var debug = false;
+    var value = 0;
+    var max;
+    var width;
+
+    var getMax = function(){
+        return $('.fp-section').length - 1
+    };
+
+    var getWidth = function(){
+        // Calculate width in percentage
+        width = (value/max) * 100;
+        width = width + '%';
+        return width;
+    };
+
+    var setWidth = function(){
+        progressBar.css({ width: getWidth() });
+    };
+
+    return {
+        init: function () {
+            if (!window.jQuery) {
+                console.warn('Error: jQuery is not loaded');
+                return
+            }
+            if (debug) console.info('ScrollIndicator initialized');
+            max = getMax()
+        },
+        update: function (section) {
+            value = section;
+            setWidth()
+        }
+    }
+})();
+
+var Slidenav = (function () {
+    var debug = false;
+    var prev = $('.slidenav__prev');
+    var next = $('.slidenav__next');
+
+    var checkDependencies = function () {
+        if (!window.jQuery) {
+            console.warn('Error: jQuery is not loaded');
+            return
+        }
+    };
+
+    var enable = function (elem) {
+        if (elem.hasClass('u-disabled')) {
+            elem.removeClass('u-disabled')
+        }
+    };
+
+    var disable = function (elem) {
+        if (!elem.hasClass('u-disabled')) {
+            elem.addClass('u-disabled')
+        }
+    };
+
+    return {
+        enablePrev: function (boolean) {
+            if (debug) console.info('Enabling Previous button', boolean);
+            checkDependencies()
+            if (boolean === true) {
+                enable(prev)
+            } else {
+                disable(prev)
+            }
+        },
+
+        enableNext: function (boolean) {
+            if (debug) console.info('Enabling Next button', boolean);
+            checkDependencies()
+            if (boolean === true) {
+                enable(next)
+            } else {
+                disable(next)
+            }
+        }
+    }
+})();
